ae0860c1c102b040655c6ba318824fc86810a736
AinaY <randrianaina.yves@gmail.com>
2023-01-26 10:08:17 +0300

    test2

--- app/main.py
+++ app/main.py
@@ -2,12 +2,12 @@ import socket
 
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
-
-    
+   
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept() # wait for client
+    #server_socket.accept() # wait for client
+
+    client, _ = server_socket.accept()  # wait for client
+    client.sendall("+PONG\r\n".encode(encoding="UTF-8"))
 
 
 if __name__ == "__main__":
