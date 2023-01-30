acca66ff98af4149272c70e3c4586d04770ed6c3
AinaY <randrianaina.yves@gmail.com>
2023-01-30 10:30:53 +0300

    stage 5

--- app/main.py
+++ app/main.py
@@ -8,10 +8,15 @@ def threaded(c):
         data = c.recv(1024).decode()
         if not data:
             break
+        data_array = data.split("\n")
+
+        if ("ECHO\r" in data_array or "echo\r" in data_array):
+            resp = f"+{data_array[-2]}\n"
+            c.send(resp.encode(encoding="UTF-8"))
         
-        c.send("+PONG\r\n".encode(encoding="UTF-8"))
-
-
+        else:
+            c.send("+PONG\r\n".encode(encoding="UTF-8"))
+        
 def main():
    
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
